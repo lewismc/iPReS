46d658ca0487e2139f0f75997d96410d8cde2827
cartermp <phillip-carter@jmstudios.net>
2014-10-19 22:33:27 -0700

    Compjure hooked up.  Working on pig-latin.

--- demo/src/demo/handler.clj
+++ demo/src/demo/handler.clj
@@ -1,10 +1,21 @@
 (ns demo.handler
   (:use compojure.core)
   (:use [ring.middleware.json :only [wrap-json-response]])
-  (:use [ring.util.response :only [response]]))
+  (:use [ring.util.response :only [response]])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
 
-(defn handler [request]
-  (response {:foo "bar"}))
+(defroutes api-routes
+  (GET "/" []
+       (response {:foo "bar"})))
 
 (def app
-  (wrap-json-response handler))
+  (->
+   (handler/api api-routes)
+   (wrap-json-response)))
+
+;;(defn handler [request]
+;;  (response {:foo "bar"}))
+;;
+;;(def app
+;;  (wrap-json-response handler))
