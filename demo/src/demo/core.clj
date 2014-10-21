c4fab02dbf9de48a9a7f91dbc44c42fbc05a8409
cartermp <phillip-carter@jmstudios.net>
2014-10-20 21:54:57 -0700

    Whoops.  Forgot to add the pig-latin...

---
+++ demo/src/demo/core.clj
@@ -0,0 +1,15 @@
+(ns demo.core)
+
+(require '[clojure.string :as str])
+
+(def vowel? (set "AaEeIiOoUu"))
+
+(defn to-pig-latin [word]
+  (let [first-letter (first word)]
+    (if (vowel? first-letter)
+      (str word "ay")
+      (str (subs word 1) first-letter "ay"))))
+
+(defn convert-message [message]
+  (let [words (str/split message #" ")]
+    (apply str (map to-pig-latin words))))
