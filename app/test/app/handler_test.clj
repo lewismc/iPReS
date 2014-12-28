dc4e7547f15ec6a5f952a6e5a0178757c75a4cd3
cartermp <phillip-carter@jmstudios.net>
2014-12-27 17:57:54 -0800

    forgot to add test file

---
+++ app/test/app/handler_test.clj
@@ -0,0 +1,19 @@
+(ns app.handler-test
+  (:require [clojure.test :refer :all]
+            [app.handler :refer :all]))
+
+(deftest is-supported?-happy
+  (testing "happy case (english)"
+           (is (= true (is-supported? "en")))))
+
+(deftest is-supported?-sad
+  (testing "sad case"
+           (is (= false (is-supported? "a")))))
+
+(deftest is-supported?-null
+  (testing "null case"
+           (is (= false (is-supported? nil)))))
+
+(deftest is-supported?-empty
+  (testing "empty case"
+           (is (= false (is-supported? "")))))
