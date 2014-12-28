af60ff6e4f7b1739d4d5ac8b444dafe683cce6a1
cartermp <phillip-carter@jmstudios.net>
2014-12-27 17:55:38 -0800

    is-supported? tests

--- app/src/app/handler.clj
+++ app/src/app/handler.clj
@@ -3,29 +3,34 @@
   (:use [ring.middleware.json])
   (:use [ring.util.response :only [response]])
 
-  (:require [compojure.hanlder :has handler]
+  (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [app.core : as core]))
+            [app.core :as core]
+            [clojure.string :as str]))
 
-(def domain ("http://podaac.jpl.nasa.gov/ws/ipres/"))
+(def domain (str "http://podaac.jpl.nasa.gov/ws/ipres/"))
 
+;; Checks if the given language code is supported.
+;; str -> bool
 (defn is-supported? [lang]
-  (contains? core/langs lang))
+  (->>
+    (keyword lang)
+    (contains? core/langs)))
 
 ;; API routes for the iPReS Service.
 (defroutes api-routes
-  (GET "/metadata/dataset"
-       (response {:msg "hello"}))
-  (GET "/metadata/granule"
-       (response {:msg "hello"}))
-  (GET "/search/dataset"
-       (response {:msg "hello"}))
-  (GET "/search/granule"
-       (response {:msg "hello"}))
-  (GET "/image/granule"
-       (response {:msg "hello"}))
-  (GET "/extract/granule"
-       (response {:msg "hello"}))
+  (GET "/metadata/dataset" [request]
+    (response {:msg "hello"}))
+  (GET "/metadata/granule" [request]
+    (response {:msg "hello"}))
+  (GET "/search/dataset" [request]
+    (response {:msg "hello"}))
+  (GET "/search/granule" [request]
+    (response {:msg "hello"}))
+  (GET "/image/granule" [request]
+    (response {:msg "hello"}))
+  (GET "/extract/granule" [request]
+    (response {:msg "hello"}))
   (route/not-found
    (response {:msg "Page not found"})))
 
--- app/test/app/core_test.clj
+++ app/test/app/core_test.clj
@@ -1,7 +1,3 @@
 (ns app.core-test
   (:require [clojure.test :refer :all]
             [app.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
