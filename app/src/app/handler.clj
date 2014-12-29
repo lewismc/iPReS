972ba30c11023fef2573f1c36841e48516ebff9f
cartermp <phillip-carter@jmstudios.net>
2014-12-28 22:35:38 -0800

    metadata route groundwork laid; boy compojure is fun

--- app/src/app/handler.clj
+++ app/src/app/handler.clj
@@ -5,41 +5,70 @@
 
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [app.core :as core]
-            [clojure.string :as str]))
+            [app.core :as core]))
 
-(def domain (str "http://podaac.jpl.nasa.gov/ws/ipres/"))
-
-;; Checks if the given language code is supported.
-;; str -> bool
-(defn is-supported? [lang]
+(defn is-supported?
+  "Return if a given language code, specified as a string,
+  is a key in the supported map of languages."
+  [lang-code]
   (->>
-    (keyword lang)
+    (keyword lang-code)
     (contains? core/langs)))
 
-;; API routes for the iPReS Service.
-(defroutes api-routes
-  (GET "/metadata/dataset" [request]
-    (response {:msg "hello"}))
-  (GET "/metadata/granule" [request]
-    (response {:msg "hello"}))
-  (GET "/search/dataset" [request]
-    (response {:msg "hello"}))
-  (GET "/search/granule" [request]
-    (response {:msg "hello"}))
-  (GET "/image/granule" [request]
-    (response {:msg "hello"}))
-  (GET "/extract/granule" [request]
-    (response {:msg "hello"}))
-  (route/not-found
-   (response {:msg "Page not found"})))
+(defn metadata-dataset-is-valid?
+  "Returns true if a request for an iPReS Metadata Dataset
+  supplies the required parameters, as detailed by:
+
+  http://podaac.jpl.nasa.gov/ws/metadata/dataset/index.html"
+  [request]
+  (and (contains? request :datasetId)
+       (contains? request :shortName)))
+
+(defn metadata-granule-is-valid?
+  "Returns true if a request for an iPRes Metadata Granule
+  supplies the required parameters, as detailed by:
+
+  http://podaac.jpl.nasa.gov/ws/metadata/granule/index.html"
+  [request]
+  (and (metadata-dataset-is-valid? request)
+       (contains? request :granuleName)))
+
+(defn api-routes
+  "The API routes for the iPReS service.
+  Nested route structure under supported languages."
+  [lang]
+  (routes
+    (GET "/metadata/dataset" [& request]
+         (if (metadata-dataset-is-valid? request)
+           (response {:msg (str "a data set for " lang)})
+           (response {:msg "you are a bad man"})))
+    (GET "/metadata/granule" [& request]
+         (if (metadata-granule-is-valid? request)
+           (response {:msg (str "a granule for " lang)})
+           (response {:msg "no kitty that's a bad kitty"})))
+    (GET "/search/dataset" [request]
+         (response {:msg "hello"}))
+    (GET "/search/granule" [request]
+         (response {:msg "hello"}))
+    (GET "/image/granule" [request]
+         (response {:msg "hello"}))
+    (GET "/extract/granule" [request]
+         (response {:msg "hello"}))
+    (route/not-found
+      (response {:msg "Page not found"}))))
+
+;; iPReS routes - 404's if language unsupported
+(defroutes ipres
+           (context "/:lang" [lang]
+                    (if (is-supported? lang)
+                      (api-routes lang))))
 
 (def app
   (->
-   (handler/api api-routes)
+    (handler/api ipres)
 
-   ;; This will need replacement...
-   (wrap-json-response)
+    ;; This will need replacement...
+    (wrap-json-response)
 
-   ;; As will this
-   (wrap-json-body)))
+    ;; As will this
+    (wrap-json-body)))
--- app/test/app/handler_test.clj
+++ app/test/app/handler_test.clj
@@ -4,16 +4,20 @@
 
 (deftest is-supported?-happy
   (testing "happy case (english)"
-           (is (= true (is-supported? "en")))))
+    (is (= true (is-supported? "en")))))
 
 (deftest is-supported?-sad
   (testing "sad case"
-           (is (= false (is-supported? "a")))))
+    (is (= false (is-supported? "a")))))
 
 (deftest is-supported?-null
   (testing "null case"
-           (is (= false (is-supported? nil)))))
+    (is (= false (is-supported? nil)))))
 
 (deftest is-supported?-empty
   (testing "empty case"
-           (is (= false (is-supported? "")))))
+    (is (= false (is-supported? "")))))
+
+(deftest is-supported?-stuff
+  (testing "weiners"
+    (is (= false is-supported? "weiners"))))
