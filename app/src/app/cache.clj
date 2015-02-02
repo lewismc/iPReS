fceb8f0b2cb22d50c5add25bbbf113a005c11e4e
cartermp <phillip-carter@jmstudios.net>
2015-02-02 08:46:34 -0800

    adding cache

--- app/src/app/cache.clj
+++ app/src/app/cache.clj
@@ -1,1 +1,40 @@
-(ns app.cache)
+(ns app.cache
+  (:require [clojure.core.cache :as cache]))
+
+(def ^:private data-cache (atom nil))
+
+(defn clear
+  "Clears the data cache, reseting it to an empty atom."
+  []
+  (reset! data-cache (cache/lru-cache-factory {})))
+
+;; Initialize the cache here to make it easier to work with.
+(clear)
+
+(defn add
+  "Adds a given key and value to the cache.
+
+  Internally, this returns a new instance of the cache
+  with the added key and value, then swaps the reference
+  of the atom to refer to this new cache."
+  [key val]
+  (swap! data-cache cache/miss key val))
+
+(defn cache-has?
+  "Returns true if the cache contains the given key; otherwise, false."
+  [key]
+  (cache/has? @data-cache key))
+
+(defn lookup
+  "Returns the item coresponding to the given key; otherwise, nil."
+  [key]
+  (cache/lookup @data-cache key))
+
+(defn drop-key
+  "Drops the given key and its associated value from the cache.
+
+  Internally, this returns a new instance of the cache
+  with the specified key and its associated value removed,
+  then swaps the reference of the atom to refer to this new cache."
+  [key]
+  (swap! data-cache cache/evict key))
